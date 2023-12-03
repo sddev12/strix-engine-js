5b8776dbdc21ad65071f5e83b6d2630a7431e057
sddev12 <sd.dev@outlook.com>
2023-12-03 13:34:21 +0000

    documentation for creating scenes

--- scenes/fortEntrance.js
+++ scenes/fortEntrance.js
@@ -1,12 +1,18 @@
 import { Scene, Option } from '../engine/scene/scene.js'
 import { game } from '../game.js'
 
+// This is how to implement a new scene
 export const fortEntrance = new Scene(
+    // Scene Name
     'Fort Entrance',
+    // Scene Text
     'You stand at the entrance of the fort staring up at the ramparts, you hear no sound.',
+    // Array of options
     [
         new Option(
+            // Option text
             "Enter the fort",
+            // Option Action
             () => {
                 game.changeScene('')
             }
