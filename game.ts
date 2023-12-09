76977140536d958c3bac1640e851efd55d1d34e5
sddev12 <sd.dev@outlook.com>
2023-12-09 12:53:16 +0000

    add default exit option to game

--- game.ts
+++ game.ts
@@ -4,9 +4,16 @@ import { fortEntrance } from './scenes/fortEntrance'
 
 // This is the entrypoint for the game
 
+function exitGame(): void {
+    process.exit(0)
+}
+
 // Here you instanciate a new game object
 // It is exported so that scenes can access the game object
 export const game = new Game()
+
+game.addDefaultOption('exit', exitGame)
+console.log(game.defaultOptions)
 
 // Add your scenes to the game object
 game.addScene(mainMenu)
